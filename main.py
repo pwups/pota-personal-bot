06f2292f6348798a679a4bcb0e728514a88a3ec3
pwups <aylapmacc@gmail.com>
2025-05-13 22:31:24 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.guilds = True
+intents.messages = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='p?', intents=intents)
+
+sticky_messages = {}
+
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def sticky(ctx, *, message: str):
+    sticky_messages[ctx.channel.id] = message
+    await ctx.send(f"sticky message set for this channel:\n> {message}")
+
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def removesticky(ctx):
+    if ctx.channel.id in sticky_messages:
+        del sticky_messages[ctx.channel.id]
+        await ctx.send("sticky message removed from this channel !")
+    else:
+        await ctx.send("no sticky message is set for this channel")
+
+@bot.command()
+async def calc(ctx, *, expression: str):
+    try:
+        result = eval(expression, {"__builtins__": {}}, {})
+        await ctx.send(f"**`{result}`**")
+    except Exception as e:
+        await ctx.send(f"Error: `{e}`")
+
+@bot.event
+async def on_member_update(before, after):
+    if before.premium_since is None and after.premium_since is not None:
+        channel = discord.utils.get(after.guild.text_channels, name="boosts")
+        if channel:
+            embed = discord.Embed(
+   description=f"_ _\n_ _⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀﹒﹒ —  {user}  ♡\n_ _⠀⠀⠀⠀⠀⠀⠀⠀⠀ <:bow_red:1371440730597363715>  __boosted__ **/pota**\n_ _⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀` check `﹕⠀*[perks](https://discord.com/channels/1319396490543890482/1371318261509263460)*\n_ _",
+                color=d63737
+            )
+            embed.set_thumbnail(url=after.avatar.url if after.avatar else after.default_avatar.url)
+            await channel.send(embed=embed)
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    if message.author.bot:
+        return
+
+    if message.channel.id in sticky_messages:
+        try:
+            await message.channel.send(sticky_messages[message.channel.id])
+        except discord.Forbidden:
+            print(f"missing perm to send sticky message in {message.channel.name}")
+
+@bot.event
+async def on_ready():
+    print(f"logged in as {bot.user}!")
+
+keep_alive()
+bot.run(TOKEN)
